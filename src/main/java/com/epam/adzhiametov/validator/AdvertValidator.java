f9985630e1307cf88cad8524089c17290039b587
Arsen_Adzhiametov <kalpazang@gmail.com>
2013-09-01 15:27:23 +0300

    javascript on checkbox added

--- src/main/java/com/epam/adzhiametov/validator/AdvertValidator.java
+++ src/main/java/com/epam/adzhiametov/validator/AdvertValidator.java
@@ -6,9 +6,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-import static com.epam.adzhiametov.util.ValidationHelper.isNameValid;
-import static com.epam.adzhiametov.util.ValidationHelper.isPhoneValid;
-import static com.epam.adzhiametov.util.ValidationHelper.isPriceValid;
+import static com.epam.adzhiametov.util.ValidationHelper.*;
 
 /**
  * Created by Arsen Adzhiametov on 7/31/13.
@@ -23,15 +21,17 @@ public class AdvertValidator implements Validator {
 
     @Override
     public void validate(Object o, Errors errors) {
-        Advert advert = (Advert)o;
-        if (!isNameValid(advert.getName())){
+        Advert advert = (Advert) o;
+        if (!isNameValid(advert.getName())) {
             errors.rejectValue("name", "error", "not matches");
         }
-        if (!isPhoneValid(advert.getPhone())){
+        if (!isPhoneValid(advert.getPhone())) {
             errors.rejectValue("phone", "error", "not matches");
         }
-        if(!isPriceValid(advert.getPrice())){
-            errors.rejectValue("price", "error", "not matches");
+        if (advert.getContract() == false) {
+            if (!isPriceValid(advert.getPrice())) {
+                errors.rejectValue("price", "error", "not matches");
+            }
         }
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "text", "error", "Field is required");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "title", "error", "Field is required");
